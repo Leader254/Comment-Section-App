0594e77b6eeb1a79d584376aa1be264763c523e5
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-05-25 14:44:09 +0300

    rejected some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ChatApp from './Components/ChatApp';
-import { Button, Stack, Typography, Dialog, DialogTitle, DialogContent } from "@mui/material"
 
 const App = () => {
   return (
